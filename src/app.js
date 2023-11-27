f53ff15e10072fb00390873c5cdfb9bae990163c
Rayane0823 <rayanemoussaoui08@gmail.com>
2023-11-27 15:14:03 +0100

    Adding Quest 2

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 require("dotenv").config();
 
-
 const app = express();
+
+app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 
@@ -10,5 +11,6 @@ app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", movieControllers.getUsers);
 app.get("/api/users/:id", movieControllers.getUserById);
-
+app.post("/api/movies", movieControllers.postMovie);
+app.post("/api/users", movieControllers.postUser);
 module.exports = app;
